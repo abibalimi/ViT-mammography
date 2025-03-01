cf959aba3fbc42f29845f5ce9ea97025571ceb4e
Olushola <abib.alimi@gmail.com>
2025-03-01 14:51:49 +0000

    preprocesor::split_train_val_test, generation of validation annotations in .csv file

--- mammography/preprocessor.py
+++ mammography/preprocessor.py
@@ -13,6 +13,7 @@ ROOT_DIR = 'raw_data/vtb-balanced-patients-202107091800'
 ROOT_JSON = 'raw_data/vtb.balanced-patients.202107091800.json'
 OUT_DIR = 'raw_data/data'
 IMAGE_ANNOTATIONS_TRAIN = 'raw_data/train_image_annotations.csv'
+IMAGE_ANNOTATIONS_VAL = 'raw_data/val_image_annotations.csv'
 TRAIN_SIZE = 0.7
 
 
@@ -67,20 +68,25 @@ def split_train_val_test(train_DirNames, val_DirNames, test_DirNames):
             image_file = long_image_file.split(sep='.')[-1]
             image_file = '.'.join([image_file,'dcm','png'])
             
-            # append annotations to list
-            #dico.append({'image' : image_file, 'label' : label})
             
-            # copy train_DirNames images to train group
+            # copy train_DirNames images to train folder
             if accessionNumber in train_DirNames:
                 # append annotations to train list
                 train_dico.append({'image' : image_file, 'label' : label})
                 copy_image_to_folder(accessionNumber, long_image_file, 'train', image_file)
-        
-          
+            
+            # copy val_DirNames images to validation folder
+            if accessionNumber in val_DirNames:
+                # append annotations to validation list
+                val_dico.append({'image' : image_file, 'label' : label})
+                copy_image_to_folder(accessionNumber, long_image_file, 'validation', image_file)
+                
+            
         # generate annotations in .csv files
         # train
         dict_to_csv(dico=train_dico, headers=['image', 'label'], file_name=IMAGE_ANNOTATIONS_TRAIN)
-        
+        # validation 
+        dict_to_csv(dico=val_dico, headers=['image', 'label'], file_name=IMAGE_ANNOTATIONS_VAL)
 
 def copy_image_to_folder(accessionNumber, long_image_file, split_dir, image_file):
     long_image_file = '.'.join([long_image_file, 'dcm', 'png'])
