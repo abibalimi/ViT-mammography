5ce72296ee04849a9da2966525f420ddbfed2ef0
Olushola <abib.alimi@gmail.com>
2025-03-02 18:58:09 +0000

    preprocesor::split_train_val_test ome cod refactoring

--- mammography/preprocessor.py
+++ mammography/preprocessor.py
@@ -4,9 +4,8 @@ import os
 import json
 import shutil
 import numpy as np
-from torchvision.io import read_image
-from PIL import Image
 from utils import dict_to_csv
+
 
 # Creating Train / Test folders (One time use)
 ROOT_DIR = 'raw_data/vtb-balanced-patients-202107091800'
@@ -16,42 +15,26 @@ IMAGE_ANNOTATIONS_TRAIN = 'raw_data/train_image_annotations.csv'
 IMAGE_ANNOTATIONS_VAL = 'raw_data/val_image_annotations.csv'
 IMAGE_ANNOTATIONS_TEST = 'raw_data/test_image_annotations.csv'
 TRAIN_SIZE = 0.7
-
-
-def split_directory_names():
-    """
-        Train-val-test split of patients' directory names:
-        since the images from the same patient shall remain within the same group (train, validation or test),
-        shuffle the names of the directories, split them (70-15-15), and return them.
-    """
-    # Get every patient's directory
-    allPatientDirNames = [f for f in os.listdir(ROOT_DIR) if f.startswith('vtb')] # helps avoid hidden files and directories
-    
-    # Shufffle all the directories
-    np.random.shuffle(allPatientDirNames)
-    
-    # Split all the directories
-    ts = int(TRAIN_SIZE * len(allPatientDirNames)) # size of train 
-    vs = (len(allPatientDirNames) - ts) // 2 # size of val (and test)
-    train_DirNames, val_DirNames, test_DirNames = allPatientDirNames[:ts], allPatientDirNames[ts:ts+vs], allPatientDirNames[ts+vs:]
-    
-    return train_DirNames, val_DirNames, test_DirNames
     
 
     
-def split_train_val_test(train_DirNames, val_DirNames, test_DirNames):
+def split_train_val_test():
     """
-        Copies the (original high-resolution) images to respective train, validation and test folders
+        Splits and copies the (original high-resolution) images to respective train, validation and test folders
         while images from the same patient remain in the same group (i.e in train, validatoin or test folder)
-        and generates a CVS file containing annotations: images and labels ("CC" or "MLO")
+        and generates a CVS file containing annotations: images and labels ("CC" or "MLO").
     """
     
-    # Make train, vali and test directories
+    # Make training, validation and test working directories
     for dirname in ['train', 'validation', 'test']:
         os.makedirs(os.path.join(OUT_DIR, dirname), exist_ok=True)
+        
+        
+    # Split patients' directory names into training, validation and test
+    train_DirNames, val_DirNames, test_DirNames = split_directory_names()
     
-
-    # Opening JSON file
+    # Split into working datasets
+    # Open JSON file
     with open(ROOT_JSON) as infile:
         data = json.load(infile)
         
@@ -85,6 +68,7 @@ def split_train_val_test(train_DirNames, val_DirNames, test_DirNames):
             if accessionNumber in test_DirNames:
                 test_dico.append({'image' : image_file, 'label' : label})
                 copy_image_to_folder(accessionNumber, long_image_file, 'test', image_file)
+        
                 
         # generate annotations in .csv files
         dict_to_csv(dico=train_dico, headers=['image', 'label'], file_name=IMAGE_ANNOTATIONS_TRAIN) # train 
@@ -92,15 +76,35 @@ def split_train_val_test(train_DirNames, val_DirNames, test_DirNames):
         dict_to_csv(dico=test_dico, headers=['image', 'label'], file_name=IMAGE_ANNOTATIONS_TEST) # test
 
 
+
+def split_directory_names():
+    """
+        Train-val-test split of patients' directory names:
+        since the images from the same patient shall remain within the same group (train, validation or test),
+        shuffle the names of the directories, split them (70-15-15), and return them.
+    """
+    # Get every patient's directory
+    allPatientDirNames = [f for f in os.listdir(ROOT_DIR) if f.startswith('vtb')] # helps avoid hidden files and directories
+    
+    # Shufffle all the directories
+    np.random.shuffle(allPatientDirNames)
+    
+    # Split all the directories
+    ts = int(TRAIN_SIZE * len(allPatientDirNames)) # size of train 
+    vs = (len(allPatientDirNames) - ts) // 2 # size of val (and test)
+    train_DirNames, val_DirNames, test_DirNames = allPatientDirNames[:ts], allPatientDirNames[ts:ts+vs], allPatientDirNames[ts+vs:]
+    
+    return train_DirNames, val_DirNames, test_DirNames
+
+
+
 def copy_image_to_folder(accessionNumber, long_image_file, split_dir, image_file):
+    """Copy image files from patient folder to a specific working dataset (split_dir) folder"""
     long_image_file = '.'.join([long_image_file, 'dcm', 'png'])
     shutil.copy(os.path.join(ROOT_DIR,accessionNumber,long_image_file), 
                                         os.path.join(OUT_DIR,split_dir,image_file))
          
          
 if __name__ == "__main__":
-    train, val, test = split_directory_names()
-    print(f'len(train) = {len(val)}')
-    
-    split_train_val_test(train, val, test)
+    split_train_val_test()
         
